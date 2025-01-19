f8f1c78e6825210440de565974bcc956f9096134
Lee Gee Young <rooftop7788@naver.com>
2025-01-19 19:35:29 +0900

    Update Router.jsx

--- src/Shared/Router.jsx
+++ src/Shared/Router.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Dex from "../pages/Dex";
-import Detail from "../pages/detail";
+import Detail from "../pages/Detail";
 import { PokeDataProvider } from "./Context";
 
 const Router = () => {
